63faf9107d5a614ee6424f4d6a0fc07b5c4469dd
Terje Berg-Hansen <terje@itfakultetet.no>
2023-09-14 19:34:51 +0200

    Eksempel på feilhåndtering

--- Unntak/src/no/itfakultetet/java/Test.java
+++ Unntak/src/no/itfakultetet/java/Test.java
@@ -1,28 +1,21 @@
 package no.itfakultetet.java;
 
-import java.io.IOException;
-
 public class Test {
-
-
-	public void run(int i) throws IOException {
-		// Test at argumentet er lik 0, hvis ikke gi feilmelding
-		if (i == 0) {
-			System.out.println("Testen var vellykket!");
-		} else {
-			throw new IOException("Testen feilet fordi parameteret var tallet "+ i +" og ikke tallet 0");
-		}
-	}
- 
 	
-	public void run(String i) throws IOException {
-		// Test at argumentet er lik 0, hvis ikke gi feilmelding
-		if (i == "0") {
-			System.out.println("Testen var vellykket!");
-		} else {
-			throw new IOException("Testen feilet fordi parameteret var teksten \"" + i + "\" og ikke tallet 0");
-		}
-	}
-	
-	
+    public void run(int i) throws IllegalArgumentException {
+        // Test at argumentet er lik 0, hvis ikke gi feilmelding
+        if (i == 0) {
+            System.out.println("Testen var vellykket!");
+        } else {
+            throw new IllegalArgumentException("Testen feilet fordi parameteret var tallet " + i + " og ikke tallet 0");
+        }
+    }
+    public void run(String i) throws IllegalArgumentException {
+        // Test at argumentet er lik 0, hvis ikke gi feilmelding
+        if (i == "0") {
+            System.out.println("Testen var vellykket!");
+        } else {
+            throw new IllegalArgumentException("Testen feilet fordi parameteret var teksten \"" + i + "\" og ikke tallet 0");
+        }
+    }
 }
