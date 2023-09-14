2d4d19dffedc26714e56cf7733ae144dc48db02e
Terje Berg-Hansen <terje@itfakultetet.no>
2023-09-14 19:33:40 +0200

    Eksempel på feilhåndtering

--- Unntak/src/no/itfakultetet/java/App.java
+++ Unntak/src/no/itfakultetet/java/App.java
@@ -1,35 +1,29 @@
 package no.itfakultetet.java;
-
-import java.io.IOException;
-
 public class App {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
+        Test test1 = new Test();
 
-		Test test1 = new Test();
-		
-		try {
-			test1.run(0);
-		} catch (IOException e) {
-			// System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		
-		try {
-			test1.run(1);
-		} catch (IOException e) {
-			//System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		
-		try {
-			test1.run("to");
-		} catch (IOException e) {
-			// System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-	
-		
-	}
+        try {
+            test1.run(0);
+        } catch (IllegalArgumentException e) {
+            // System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
 
+        try {
+            test1.run(1);
+        } catch (IllegalArgumentException e) {
+            //System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            test1.run("to");
+        } catch (IllegalArgumentException e) {
+            // System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
+
